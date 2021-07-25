83002b44cb7b4ce278fe415fe66881b56ed7e42f
Jeremiah Farthing <jeremiah.farthing@gmail.com>
2021-07-25 00:20:35 -0600

    created framework

--- schema.sql
+++ schema.sql
@@ -1,1 +1,31 @@
-DROP DATABASE IF EXISTS 
+DROP DATABASE IF EXISTS company_DB;
+CREATE DATABASE company_DB;
+
+USE company_DB;
+
+CREATE TABLE department (
+  id INT NOT NULL,
+  name VARCHAR(30) NOT NULL, /* to hold department name */
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE role (
+  id INT NOT NULL,
+  title  VARCHAR(30) NOT NULL, /* to hold role title */
+  salary  DECIMAL NOT NULL, /* to hold role salary */
+  department_id  INT NOT NULL, /* to hold reference to department role belongs to */
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE employee (
+  id INT NOT NULL,
+  first_name VARCHAR(30) NOT NULL, /* to hold employee first name */
+  last_name VARCHAR(30) NOT NULL, /* to hold employee last name */
+  role_id INT NOT NULL, /* to hold reference to role employee has */ 
+  manager_id INT NOT NULL, /* to hold reference to another employee that manages the employee being Created. This field may be null if the employee has no manager */
+  PRIMARY KEY (id)
+);
+
+SELECT * FROM department;
+SELECT * FROM employee;
+SELECT * FROM role;
