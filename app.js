d4fd1985956e9584754de973fb9ade7da7dea702
Jeremiah Farthing <jeremiah.farthing@gmail.com>
2021-07-25 17:53:39 -0600

    getting app ready for multiple queries

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var inquirer = require("inquirer");
 const mysql = require('mysql');
+
 const connection = mysql.createConnection({
     host: 'localhost',
     // Your port; if not 3306
@@ -14,33 +15,56 @@ const connection = mysql.createConnection({
 connection.connect(async (err) => {
     if (err) throw err;
     console.log(`connected as id ${connection.threadId}\n`);
-    start();
 });
-const start = async () => {
-    try {
-        const userChoice1 = await inquirer.prompt([
-            {
-              name: 'userOption',
-              type: 'list',
-              message: 'What would you like to do?',
-              choices: [
-                new inquirer.Separator(),
-                'View Employees',
-                'View Departments',
-                'View Roles',
-                new inquirer.Separator(),
-                'Add Employee',
-                'Add Department',
-                'Add Role',
-                new inquirer.Separator(),
-                'Update Employee Role',
-                new inquirer.Separator(),
-                'Exit System'
-              ],
+// Start the app
+const start = () => {
+    inquirer
+        .prompt({
+            name: 'userOption',
+            type: 'list',
+            message: 'What would you like to do?',
+            choices: [
+              new inquirer.Separator(),
+              'View Employees',
+              'View Departments',
+              'View Roles',
+              new inquirer.Separator(),
+              'Add Employee',
+              'Add Department',
+              'Add Role',
+              new inquirer.Separator(),
+              'Update Employee Role',
+              new inquirer.Separator(),
+              'Exit System'
+            ],
+        })
+        .then((answer) => {
+            switch (answer.userOption) {
+                // View
+                case 'View Employees':
+                    break;
+                case 'View Departments':
+                    break;
+                case 'View Roles':
+                    break;
+                // Add
+                case 'Add Employee':
+                    break;
+                case 'Add Department':
+                    break;
+                case 'Add Role':
+                    break;
+                // Update
+                case 'Update Employee Role':
+                    break;
+                // Exit
+                case 'Exit System':
+                    console.log("Thanks for using the Employee Tracker. Goodbye.")
+                    connection.end();
+                    break;
+                // Creates an error message if something breaks
+                default:
+                    console.log(`Uh oh, you broke me! Tell my creator how I died.`);
             }
-        ]);
-        doWhatUserWantsTodo(userChoice1.userOption);
-      } catch (e) {
-        console.log(e);
-      }
+        })
 };
